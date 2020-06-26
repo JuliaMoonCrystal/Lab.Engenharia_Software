81425eb30f513cbabcc2959008e72ad4980fdced
JuliaMoonCrystal <47614833+JuliaMoonCrystal@users.noreply.github.com>
2020-06-25 21:20:01 -0300

    Add files via upload

--- E-learning/src/Atores/ADM.java
+++ E-learning/src/Atores/ADM.java
@@ -1,5 +1,19 @@
 package Atores;
 
 public class ADM {
-
+	private String Senha_ADM;
+    private String Login_ADM;
+    
+	public String getSenha_ADM() {
+		return Senha_ADM;
+	}
+	public void setSenha_ADM(String senha_ADM) {
+		Senha_ADM = senha_ADM;
+	}
+	public String getLogin_ADM() {
+		return Login_ADM;
+	}
+	public void setLogin_ADM(String login_ADM) {
+		Login_ADM = login_ADM;
+	}
 }
